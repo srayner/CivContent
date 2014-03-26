245377d3bc99fe23e6a6ae3bf1776410041344d2
Steve Rayner <srayner02@googlemail.com>
2014-03-26 21:22:37 +0000

    Added mysql database schema.

---
+++ data/mysql_schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS `content_category` (
+  `content_category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`content_category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `content_post` (
+  `content_post_id` int(11) NOT NULL AUTO_INCREMENT,
+  `content_category_id` int(11) NOT NULL,
+  `post_title` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `post_body` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `post_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`content_post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+ALTER TABLE `content_post`
+  ADD CONSTRAINT `fk_content_category` FOREIGN KEY (`content_post_id`) REFERENCES `content_category` (`content_category_id`);
